e113567554549c89135f0e03ceddd3ffc07c41aa
Denys Bobrovnikov <nitrolog@gmail.com>
2016-03-29 13:07:11 +0300

    JS_Homework_5-6

---
+++ Tech_Skills/JS.Module_5-6/js/script.js
@@ -0,0 +1,98 @@
+var startPauseButton = document.querySelector('.start-pause-button p');
+var clearButton = document.querySelector('.clear-button');
+var timerHours = document.querySelector('.timer__hours');
+var timerMilliseconds = document.querySelector('.timer__milliseconds');
+var timer;
+var hours = 0, minutes = 0, seconds = 0, milliseconds = 0;
+var clickChecker = false;
+var pauseChecker = true;
+
+function startButtonHandler() {
+  startPauseButton.innerHTML = 'Pause';
+  clickChecker = true;
+
+  function startTimer() {
+    milliseconds++;
+
+    if (milliseconds > 99) {
+      milliseconds = 0;
+      milliseconds = '0';
+      seconds++;
+    };
+
+    if (seconds == 0) {
+      seconds = '00';
+    };
+
+    if (seconds > 0 && seconds <= 9) {
+      seconds = '0' + +seconds;
+    };
+
+    if (seconds > 59) {
+      seconds = '00';
+      seconds = 0;
+      minutes++;
+    };
+
+    if (minutes == 0) {
+      minutes = '00';
+    };
+
+    if (minutes > 59) {
+      minutes = '00';
+      minutes = 0;
+      hours++;
+    } else {
+      minutes = '0' + +minutes;
+    };
+
+    if (hours == 0) {
+      hours = '00';
+    };
+
+    if (hours > 99) {
+      hours = '00';
+      hours = 0;
+    } else {
+      hours = '0' + +hours;
+    };
+
+    timerMilliseconds.innerHTML = milliseconds;
+    timerHours.innerHTML = hours + ':' + minutes + ':' + seconds;
+
+  };
+
+  timer = setInterval(startTimer, 10);
+}
+
+function clearTimerButtonHandler() {
+  clearInterval(timer);
+  clickChecker = false;
+  pauseChecker = true;
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+  milliseconds = 0;
+  timerMilliseconds.innerHTML = '0';
+  timerHours.innerHTML = '00:00:00';
+  startPauseButton.innerHTML = 'Start';
+  startPauseButton.removeEventListener('click', startButtonHandler);
+}
+
+function startPauseButtonChecker() {
+  if (!clickChecker) {
+    console.log(pauseChecker);
+    startButtonHandler();
+  } else { if (!pauseChecker) {
+      startButtonHandler();
+      pauseChecker = true;
+    } else {
+      startPauseButton.innerHTML = 'Cont..';
+      clearInterval(timer);
+      pauseChecker = false;
+    }
+  }
+}
+
+startPauseButton.addEventListener('click', startPauseButtonChecker);
+clearButton.addEventListener('click', clearTimerButtonHandler);
