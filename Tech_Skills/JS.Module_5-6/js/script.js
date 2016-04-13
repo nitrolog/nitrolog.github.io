04484594b00fbb38d8c58a4cd10910e29db2381e
Denys Bobrovnikov <nitrolog@gmail.com>
2016-04-13 19:04:56 +0300

    JS_Homework_5-6

--- Tech_Skills/JS.Module_5-6/js/script.js
+++ Tech_Skills/JS.Module_5-6/js/script.js
@@ -3,89 +3,92 @@ var clearButton = document.querySelector('.clear-button');
 var timerHours = document.querySelector('.timer__hours');
 var timerMilliseconds = document.querySelector('.timer__milliseconds');
 var timer;
-var hours = 0, minutes = 0, seconds = 0, milliseconds = 0;
 var clickChecker = false;
 var pauseChecker = true;
+var date, contDate, pauseDate, currentDate, timerDate;
+
 
 function startButtonHandler() {
+
   startPauseButton.innerHTML = 'Pause';
   clickChecker = true;
 
   function startTimer() {
-    milliseconds = +milliseconds + 4;
 
-    if (milliseconds > 999) {
-      milliseconds = 0;
-      milliseconds = '0';
-      seconds++;
-    };
+    var hours, minutes, seconds, milliseconds;
+
+    currentDate = new Date();
+
+    currentDate.setMilliseconds( currentDate.getMilliseconds() - date.getMilliseconds() );
+    currentDate.setSeconds( currentDate.getSeconds() - date.getSeconds() );
+    currentDate.setMinutes( currentDate.getMinutes() - date.getMinutes() );
+    currentDate.setHours( currentDate.getHours() - date.getHours() );
+
+    milliseconds = currentDate.getMilliseconds();
+    seconds = currentDate.getSeconds();
+    minutes = currentDate.getMinutes();
+    hours = currentDate.getHours();
 
     if (seconds == 0) {
       seconds = '00';
-    };
+    }
 
     if (seconds > 0 && seconds <= 9) {
-      seconds = '0' + +seconds;
-    };
-
-    if (seconds > 59) {
-      seconds = '00';
-      seconds = 0;
-      minutes++;
-    };
+      seconds = '0' + seconds;
+    }
 
     if (minutes == 0) {
       minutes = '00';
-    };
+    }
 
-    if (minutes > 59) {
-      minutes = '00';
-      minutes = 0;
-      hours++;
-    } else {
-      minutes = '0' + +minutes;
-    };
+    if (minutes > 0 && minutes <= 9) {
+      minutes = '0' + minutes;
+    }
 
     if (hours == 0) {
       hours = '00';
-    };
+    }
 
-    if (hours > 99) {
-      hours = '00';
-      hours = 0;
-    } else {
-      hours = '0' + +hours;
-    };
+    if (hours > 0 && hours <= 9) {
+      hours = '0' + hours;
+    }
 
     timerMilliseconds.innerHTML = milliseconds;
     timerHours.innerHTML = hours + ':' + minutes + ':' + seconds;
 
-  };
+  }
 
-  timer = setInterval(startTimer, 4);
+  timer = setInterval(startTimer, 25);
 }
 
 function clearTimerButtonHandler() {
   clearInterval(timer);
   clickChecker = false;
   pauseChecker = true;
-  hours = 0;
-  minutes = 0;
-  seconds = 0;
-  milliseconds = 0;
   timerMilliseconds.innerHTML = '0';
   timerHours.innerHTML = '00:00:00';
   startPauseButton.innerHTML = 'Start';
 }
 
+function differenceDate() {
+  date.setMilliseconds( date.getMilliseconds() - ( pauseDate.getMilliseconds() - contDate.getMilliseconds() ));
+  date.setSeconds( date.getSeconds() - ( pauseDate.getSeconds() - contDate.getSeconds() ));
+  date.setMinutes( date.getMinutes() - ( pauseDate.getMinutes() - contDate.getMinutes() ));
+  date.setHours( date.getHours() - ( pauseDate.getHours() - contDate.getHours() ));
+}
+
 function startPauseButtonChecker() {
   if (!clickChecker) {
+    date = new Date();
     startButtonHandler();
   } else { if (!pauseChecker) {
+      contDate = new Date();
+      differenceDate();
       startButtonHandler();
       pauseChecker = true;
     } else {
       startPauseButton.innerHTML = 'Cont..';
+      pauseDate = new Date();
       clearInterval(timer);
       pauseChecker = false;
     }
