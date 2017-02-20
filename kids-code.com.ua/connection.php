57c927b863646cdabf4192a1ade7dd9e00c7301d
Denys Bobrovnikov <nitrolog@gmail.com>
2017-02-20 23:35:26 +0200

    Update

--- kids-code.com.ua/connection.php
+++ kids-code.com.ua/connection.php
@@ -2,9 +2,9 @@
  function Connect()
 {
  $dbhost = "localhost";
- $dbuser = "u_kidscode";
+ $dbuser = "b1957729_kids";
  $dbpass = "EY7KeSuM";
- $dbname = "kidscode";
+ $dbname = "b1957729_kidscode";
  
  // Create connection
  $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
